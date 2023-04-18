8a381ec7658042bafbbe8c057d4016fbadbd766c
Vinh Tong <vinhtong.dev@gmail.com>
2023-04-18 11:58:17 +0700

    Create folders by code

---
+++ myapp/init-celeritas.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"github.com/tongvinh/celeritas"
+	"log"
+	"os"
+)
+
+func initApplication() *application {
+	path, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// init celeritas
+	cel := &celeritas.Celeritas{}
+	err = cel.New(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cel.AppName = "myapp"
+	cel.Debug = true
+
+	app := &application{
+		App: cel,
+	}
+
+	return app
+}
--- myapp/vendor/github.com/tongvinh/celeritas/celeritas.go
+++ myapp/vendor/github.com/tongvinh/celeritas/celeritas.go
@@ -1,13 +1,34 @@
 package celeritas
 
-func TestFunc(a, b int) int {
-	return a + b
+const version = "1.0.0"
+
+type Celeritas struct {
+	AppName string
+	Debug   bool
+	Version string
 }
 
-func TestFunc2(a, b int) int {
-	return a - b
+func (c *Celeritas) New(rootPath string) error {
+	pathConfig := initPaths{
+		rootPath:    rootPath,
+		folderNames: []string{"handlers", "migrations", "views", "data", "public", "tmp", "logs", "middleware"},
+	}
+
+	err := c.Init(pathConfig)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
-func TestFunc3(a, b int) int {
-	return a * b
+func (c *Celeritas) Init(p initPaths) error {
+	root := p.rootPath
+	for _, path := range p.folderNames {
+		// Create folder if it doesn't exist
+		err := c.CreateDirIfNotExist(root + "/" + path)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
