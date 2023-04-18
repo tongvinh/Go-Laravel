b6fba7656cffd7348ea1055c529e81280adcf35a
Vinh Tong <vinhtong.dev@gmail.com>
2023-04-18 16:19:25 +0700

    Implement RenderPage in Celeritas Package

--- celeritas/routes.go
+++ celeritas/routes.go
@@ -1,7 +1,6 @@
 package celeritas
 
 import (
-	"fmt"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"net/http"
@@ -15,10 +14,6 @@ func (c *Celeritas) routes() http.Handler {
 		mux.Use(middleware.Logger)
 	}
 	mux.Use(middleware.Recoverer)
-
-	mux.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Welcome to Celeritas")
-	})
 
 	return mux
 }
