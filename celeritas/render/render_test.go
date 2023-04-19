fff2bbd8dbe5e4c8ab2c8dde1aef8ad85b61f296
Vinh Tong <vinhtong.dev@gmail.com>
2023-04-19 13:16:19 +0700

    Simplifying our tests using table test

--- celeritas/render/render_test.go
+++ celeritas/render/render_test.go
@@ -6,42 +6,43 @@ import (
 	"testing"
 )
 
+var pageData = []struct {
+	name          string
+	renderer      string
+	template      string
+	errorExpected bool
+	errorMessage  string
+}{
+	{"go_page", "go", "home", false, "error rendering go template"},
+	{"go_page_no_template", "go", "no-file", true, "no error rendering go template, when one is expected"},
+	{"jet_page", "jet", "home", false, "error rendering jet template"},
+	{"jet_page_no_template", "jet", "no-file", true, "no error rendering jet template, when one is expected"},
+	{"invalid_render_engine", "foo", "home", true, "no error rendering with non-existent template engine"},
+}
+
 func TestRender_Page(t *testing.T) {
-	r, err := http.NewRequest("GET", "/some-url", nil)
-	if err != nil {
-		t.Error(err)
-	}
 
-	w := httptest.NewRecorder()
+	for _, e := range pageData {
+		r, err := http.NewRequest("GET", "/some-url", nil)
+		if err != nil {
+			t.Error(err)
+		}
 
-	testRenderer.Renderer = "go"
-	testRenderer.RootPath = "./testdata"
+		w := httptest.NewRecorder()
 
-	err = testRenderer.Page(w, r, "home", nil, nil)
-	if err != nil {
-		t.Error("Error rendering page", err)
-	}
+		testRenderer.Renderer = e.renderer
+		testRenderer.RootPath = "./testdata"
 
-	err = testRenderer.Page(w, r, "no-file", nil, nil)
-	if err == nil {
-		t.Error("Error rendering non-existent template", err)
-	}
-
-	testRenderer.Renderer = "jet"
-	err = testRenderer.Page(w, r, "home", nil, nil)
-	if err != nil {
-		t.Error("Error rendering page", err)
-	}
-
-	err = testRenderer.Page(w, r, "no-file", nil, nil)
-	if err == nil {
-		t.Error("Error rendering non-existent jet template", err)
-	}
-
-	testRenderer.Renderer = ""
-	err = testRenderer.Page(w, r, "home", nil, nil)
-	if err == nil {
-		t.Error("No error rendering while rendering with invalid renderer specified", err)
+		err = testRenderer.Page(w, r, e.template, nil, nil)
+		if e.errorExpected {
+			if err == nil {
+				t.Errorf("%s: %s", e.name, e.errorMessage)
+			}
+		} else {
+			if err != nil {
+				t.Errorf("%s: %s: %s", e.name, e.errorMessage, err.Error())
+			}
+		}
 	}
 }
 
