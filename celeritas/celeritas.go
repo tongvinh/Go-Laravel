d1d6bb2d842cee124c6f95e0a84c84cee8abbf58
Vinh Tong <vinhtong.dev@gmail.com>
2023-04-19 14:48:54 +0700

    Add middleware session

--- celeritas/celeritas.go
+++ celeritas/celeritas.go
@@ -80,6 +80,7 @@ func (c *Celeritas) New(rootPath string) error {
 			lifetime: os.Getenv("COOKIE_LIFETIME"),
 			persist:  os.Getenv("COOKIE_PERSIST"),
 			secure:   os.Getenv("COOKIE_SECURE"),
+			domain:   os.Getenv("COOKIE_DOMAIN"),
 		},
 		sessionType: os.Getenv("SESSION_TYPE"),
 	}
@@ -90,6 +91,7 @@ func (c *Celeritas) New(rootPath string) error {
 		CookiePersist:  c.config.cookie.persist,
 		CookieName:     c.config.cookie.name,
 		SessionType:    c.config.sessionType,
+		CookieDomain:   c.config.cookie.domain,
 	}
 
 	c.Session = sess.InitSession()
