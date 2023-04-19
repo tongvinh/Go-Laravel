402685a21d91e4b0a841c7ba6d7dee51baf70fec
Vinh Tong <vinhtong.dev@gmail.com>
2023-04-19 13:02:23 +0700

    write more testing for render

--- celeritas/render/render_test.go
+++ celeritas/render/render_test.go
@@ -19,12 +19,60 @@ func TestRender_Page(t *testing.T) {
 
 	err = testRenderer.Page(w, r, "home", nil, nil)
 	if err != nil {
-		t.Error(err)
+		t.Error("Error rendering page", err)
+	}
+
+	err = testRenderer.Page(w, r, "no-file", nil, nil)
+	if err == nil {
+		t.Error("Error rendering non-existent template", err)
 	}
 
 	testRenderer.Renderer = "jet"
 	err = testRenderer.Page(w, r, "home", nil, nil)
 	if err != nil {
+		t.Error("Error rendering page", err)
+	}
+
+	err = testRenderer.Page(w, r, "no-file", nil, nil)
+	if err == nil {
+		t.Error("Error rendering non-existent jet template", err)
+	}
+
+	testRenderer.Renderer = ""
+	err = testRenderer.Page(w, r, "home", nil, nil)
+	if err == nil {
+		t.Error("No error rendering while rendering with invalid renderer specified", err)
+	}
+}
+
+func TestRender_GoPage(t *testing.T) {
+	w := httptest.NewRecorder()
+	r, err := http.NewRequest("GET", "/url", nil)
+	if err != nil {
 		t.Error(err)
+	}
+
+	testRenderer.Renderer = "go"
+	testRenderer.RootPath = "./testdata"
+
+	err = testRenderer.Page(w, r, "home", nil, nil)
+	if err != nil {
+		t.Error("Error rending page", err)
+	}
+}
+
+func TestRender_JetPage(t *testing.T) {
+	w := httptest.NewRecorder()
+	r, err := http.NewRequest("GET", "/url", nil)
+	if err != nil {
+		t.Error(err)
+	}
+
+	testRenderer.Renderer = "jet"
+	testRenderer.RootPath = "./testdata"
+
+	err = testRenderer.Page(w, r, "home", nil, nil)
+	if err != nil {
+		t.Error("Error rending page", err)
 	}
 }
