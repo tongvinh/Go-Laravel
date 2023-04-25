baac7febb21b864637572ecabe53ea7c4603b391
Vinh Tong <vinhtong.dev@gmail.com>
2023-04-25 13:38:00 +0700

    run migrate

--- celeritas/cmd/cli/helpers.go
+++ celeritas/cmd/cli/helpers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/joho/godotenv"
 	"os"
 )
@@ -18,4 +19,34 @@ func setup() {
 
 	cel.RootPath = path
 	cel.DB.DataType = os.Getenv("DATABASE_TYPE")
+}
+
+func getDSN() string {
+	dbType := cel.DB.DataType
+
+	if dbType == "pgx" {
+		dbType = "postgres"
+	}
+
+	if dbType == "postgres" {
+		var dsn string
+		if os.Getenv("DATABASE_PASS") != "" {
+			dsn = fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=%s",
+				os.Getenv("DATABASE_USER"),
+				os.Getenv("DATABASE_PASS"),
+				os.Getenv("DATABASE_HOST"),
+				os.Getenv("DATABASE_PORT"),
+				os.Getenv("DATABASE_NAME"),
+				os.Getenv("DATABASE_SSL_MODE"))
+		} else {
+			dsn = fmt.Sprintf("postgres://%s@%s:%s/%s?sslmode=%s",
+				os.Getenv("DATABASE_USER"),
+				os.Getenv("DATABASE_HOST"),
+				os.Getenv("DATABASE_PORT"),
+				os.Getenv("DATABASE_NAME"),
+				os.Getenv("DATABASE_SSL_MODE"))
+		}
+		return dsn
+	}
+	return "mysql://" + cel.BuildDSN()
 }
--- celeritas/cmd/cli/main.go
+++ celeritas/cmd/cli/main.go
@@ -4,7 +4,6 @@ import (
 	"errors"
 	"github.com/fatih/color"
 	"github.com/tongvinh/celeritas"
-	"log"
 	"os"
 )
 
@@ -13,6 +12,7 @@ const version = "1.0.0"
 var cel celeritas.Celeritas
 
 func main() {
+	var message string
 	arg1, arg2, arg3, err := validateInput()
 	if err != nil {
 		exitGracefully(err)
@@ -26,6 +26,16 @@ func main() {
 	case "version":
 		color.Yellow("Application version: " + version)
 
+	case "migrate":
+		if arg2 == "" {
+			arg2 = "up"
+		}
+		err = doMigrate(arg2, arg3)
+		if err != nil {
+			exitGracefully(err)
+		}
+		message = "Migrations complete!"
+
 	case "make":
 		if arg2 == "" {
 			exitGracefully(errors.New("make requires a subcommand: (migration|model|handler)"))
@@ -36,8 +46,10 @@ func main() {
 		}
 
 	default:
-		log.Println(arg2, arg3)
+		showHelp()
 	}
+
+	exitGracefully(nil, message)
 }
 
 func validateInput() (string, string, string, error) {
