37a0bbffaf0425dabcb84140d63c11f77ad7692c
Vinh Tong <vinhtong.dev@gmail.com>
2023-04-27 14:34:08 +0700

    function helper for routes

--- myapp/routes.go
+++ myapp/routes.go
@@ -12,13 +12,13 @@ func (a *application) routes() *chi.Mux {
 	// Middleware must come before any routes
 
 	// add routes here
-	a.App.Routes.Get("/", a.Handlers.Home)
+	a.get("/", a.Handlers.Home)
 	a.App.Routes.Get("/go-page", a.Handlers.GoPage)
 	a.App.Routes.Get("/jet-page", a.Handlers.JetPage)
 	a.App.Routes.Get("/sessions", a.Handlers.SessionTest)
 
 	a.App.Routes.Get("/users/login", a.Handlers.UserLogin)
-	a.App.Routes.Post("/users/login", a.Handlers.PostUserLogin)
+	a.post("/users/login", a.Handlers.PostUserLogin)
 	a.App.Routes.Post("/users/logout", a.Handlers.Logout)
 
 	a.App.Routes.Get("/form", a.Handlers.Form)
